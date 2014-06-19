395b9d08d001bb67080b945d485991dea7dc6ef2
Corey Leigh Latislaw <colabug@gmail.com>
2014-06-19 00:35:48 -0500

    Hello world

---
+++ src/com/colabug/dmc/lc/LooselyCoupledActivity.java
@@ -0,0 +1,17 @@
+package com.colabug.dmc.lc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LooselyCoupledActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}
